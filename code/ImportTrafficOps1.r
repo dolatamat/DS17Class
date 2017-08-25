31844768715fe3c656bd861c53bb2f50624f9b87
Mat Dolata <dolatamat@hotmail.com>
2017-08-25 13:58:10 -0700

    Fix ImportTrafficOps

---
+++ code/ImportTrafficOps1.r
@@ -0,0 +1,7 @@
+install.packages("readxl")
+library(readxl)
+Summary_of_Traffic_Operations_1_ <- read_excel("C:/Users/mjd/Downloads/Summary of Traffic Operations (1).xlsx")
+View(Summary_of_Traffic_Operations_1_)
+Ops1 <- Summary_of_Traffic_Operations_1_
+Ops1
+#dummytext
